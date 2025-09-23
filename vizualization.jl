817c946628f456a11037b6b4fc0a66c19a87df1d
Raphael-Tresor <40422324+Raphael-Tresor@users.noreply.github.com>
2025-09-23 17:49:24 +0200

    2D projection triangle

--- vizualization.jl
+++ vizualization.jl
@@ -120,16 +120,21 @@ savefig(p, "sphere_grid.png")
 # --- Create 2D projection plot ---
 # For each point on the sphere, compute (arcsin(z/R), atan(y,x))
 p2 = plot(legend=false, grid=true, framestyle=:box, size=(900, 600),
-          xlabel="φ = atan(y,x)", ylabel="θ' = arcsin(z/R)",
-          xlims=(-π-0.2, π+0.2), ylims=(-π/2-0.2, π/2+0.2),
-          aspect_ratio=:equal)
+          xlabel="φ = atan(y,x)", ylabel="θ' = arcsin(z)",
+          xlims=(-π+0.02, π+0.02), ylims=(-π/2-0.05, π/2+0.05),
+          aspect_ratio=:equal, 
+          xticks = (-π:π/2:π, ["-π", "-π/2", "0", "π/2", "π"]), 
+          yticks = (-π/2:π/4:π/2, ["-π/2", "-π/4", "0", "π/4", "π/2"]),)
 
 # Project poles
 scatter!(p2, [0.0], [π/2], mc=:black, ms=4.0)  # North pole: z/R = 1
 scatter!(p2, [0.0], [-π/2], mc=:black, ms=4.0) # South pole: z/R = -1
 
-# Project rings of points
+
+rings = [] # To store the projection of each ring of points
+
 for r in 1:(levels-2)
+    ring = []
     θ = r*dθ
     proj_x = Float64[]
     proj_y = Float64[]
@@ -139,11 +144,75 @@ for r in 1:(levels-2)
         # Compute projection: (arcsin(z/R), atan(y,x))
         θ_proj = asin(z)
         φ_proj = atan(y, x)
+        # Store projected points for scatter plot
         push!(proj_x, φ_proj)
         push!(proj_y, θ_proj)
+        # Reporting the different values on the abcissa and ordinate axes
+        push!(ring, (φ_proj, θ_proj))
     end
+    push!(rings, ring)
     scatter!(p2, proj_x, proj_y, mc=:black, ms=4.0)
 end
+
+# North and south pole
+north_pole = (0.0, π/2)
+south_pole = (0.0, -π/2)
+
+# Fill triangles between north pole and first ring
+for i in 1:n_long
+    point1 = north_pole
+    point2 = rings[1][i]
+    point3 = rings[1][mod1(i+1, n_long)]
+    plot!(p2, [point1[1], point2[1], point3[1]], [point1[2], point2[2], point3[2]], seriestype=:shape, fillcolor=RGBA(0.8,0.8,0.8,0.4), linecolor=:auto, label=false)
+end
+
+
+# Fill triangles between last ring and south pole
+for i in 1:n_long
+    point1 = rings[end][i]
+    point2 = rings[end][mod1(i+1, n_long)]
+    point3 = south_pole
+    plot!(p2, [point1[1], point2[1], point3[1]], [point1[2], point2[2], point3[2]], seriestype=:shape, fillcolor=RGBA(0.8,0.8,0.8,0.4), linecolor=:auto, label=false)
+end
+
+
+# Fill triangles between rings
+for r in 1:(length(rings)-1)
+    ringA = rings[r]
+    ringB = rings[r+1]
+    for i in 1:n_long
+        if abs(ringA[i][1] - ringA[mod1(i+1, n_long)][1]) < π/4.0
+            a1 = ringA[i]
+            a2 = ringA[mod1(i+1, n_long)]
+            b1 = ringB[i]
+            b2 = ringB[mod1(i+1, n_long)]
+            # Two triangles per quad
+            plot!(p2, [a1[1], b1[1], b2[1]], [a1[2], b1[2], b2[2]], seriestype=:shape, fillcolor=RGBA(0.2,0.2,0.2,0.4), linecolor=:auto, label=false)
+            plot!(p2, [a1[1], b2[1], a2[1]], [a1[2], b2[2], a2[2]], seriestype=:shape, fillcolor=RGBA(0.4,0.4,0.4,0.4), linecolor=:auto, label=false)
+        end
+    end
+end
+
+
+#Fill a fake triangles to cover the discontinuity at φ = ±π
+for r in 1:(length(rings)-1)
+    ringA = rings[r]
+    ringB = rings[r+1]
+    max = maximum([rings[r][i][1] for i in 1:n_long])
+    min = minimum([rings[r][i][1] for i in 1:n_long])
+    fake_positive = max + abs(ringA[1][1] - ringA[2][1])
+    fake_negative = min -abs(ringA[1][1] - ringA[2][1])
+    ringA_y = ringA[1][2]
+    ringB_y = ringB[1][2]
+    # Two triangles per quad
+    plot!(p2, [max, fake_positive, fake_positive], [ringA_y, ringA_y, ringB_y], seriestype=:shape, fillcolor=RGBA(0.2,0.2,0.2,0.4), linecolor=:auto, label=false)
+    plot!(p2, [max, fake_positive, max], [ringA_y, ringB_y, ringB_y], seriestype=:shape, fillcolor=RGBA(0.4,0.4,0.4,0.4), linecolor=:auto, label=false)
+    plot!(p2, [min, fake_negative, fake_negative], [ringA_y, ringA_y, ringB_y], seriestype=:shape, fillcolor=RGBA(0.2,0.2,0.2,0.4), linecolor=:auto, label=false)
+    plot!(p2, [min, fake_negative,min], [ringA_y, ringB_y, ringB_y], seriestype=:shape, fillcolor=RGBA(0.4,0.4,0.4,0.4), linecolor=:auto, label=false)
+end
+
+
+
 
 # Only project the points, no grid lines or geodesics
 savefig(p2, "sphere_projection.png")
